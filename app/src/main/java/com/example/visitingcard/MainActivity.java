a964c6178a3243ac79109cf5803a2bd38a1c3365
AzeezFazry <azeezfazry@gmail.com>
2021-05-24 22:19:04 +0530

    Basic android app to generate a visiting card

---
+++ app/src/main/java/com/example/visitingcard/MainActivity.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Azeez Fazry on May 24, 2021, 7:23:42 PM
+ *
+ *      git: github.com/azeez-fazry
+ */
+package com.example.visitingcard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
